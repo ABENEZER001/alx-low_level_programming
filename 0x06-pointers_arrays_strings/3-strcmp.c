ee030390bd94026519dc8567941cee1a9aa8acf1
Abenezer <abenezerbisrat33@gmail.com>
2023-03-03 12:26:18 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 /**
  * _strcmp - compare sring values
  * @s1: input value
  * @s2: input value
  * Return: s1[i] - s2[i]
  */
-int *_strncmp(char s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
@@ -16,7 +15,7 @@ int *_strncmp(char s1, char *s2)
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
-	}
+		}
 		i++;
 	}
 	return (0);
